087dbfdce6e77bc80ce53e891b5f4ea5c700732b
Romain Morlevat <romainmorlevat@gmail.com>
2018-08-30 13:36:01 +0200

    Third try with searching
    A different approch. I made distinction between arrays based on their
    lenghts and used a search.
    Sadly it's not faster than the first try.
    > Finished in 0.00111 seconds (files took 0.06651 seconds to load)

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -3,13 +3,19 @@ def intersect(array1, array2)
 
   return result if array1.empty? || array2.empty?
 
-  array1.each do |el1|
-    array2.each_with_index do |el2, el2index|
-      next unless el1 == el2
-      result << el1
-      array2.delete_at(el2index)
-      break
-    end
+  if array1.length > array2.length
+    shortest_array = array2
+    longest_array = array1
+  else
+    shortest_array = array1
+    longest_array = array2
+  end
+
+  longest_array.each do |el1|
+    break if shortest_array.empty?
+    next unless shortest_array.include?(el1)
+    result << el1
+    shortest_array.delete_at(shortest_array.find_index(el1))
   end
 
   result
